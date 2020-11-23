9db45996c885d3eefd40aa9325549cbfb6243f7d
19980402 <74659985+19980402@users.noreply.github.com>
2020-11-23 11:28:40 +0800

    Update 112301.App.java

--- 112301.App.java
+++ 112301.App.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class shuji {
+public class suiji {
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
         while (true){
