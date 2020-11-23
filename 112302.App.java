565d4bd894fa719435f4c2c73c85a94d24ac1ac2
19980402 <74659985+19980402@users.noreply.github.com>
2020-11-23 12:04:38 +0800

    java随机生成坐标

---
+++ 112302.App.java
@@ -0,0 +1,10 @@
+public class suiji {
+    public static void main(String[] args) throws InterruptedException{
+        while (true){
+            Thread.sleep(1000);
+                int x=(int) (Math.random()*100%100);
+                int y=(int) (Math.random()*100%100);
+                System.out.println(x+","+y);
+        }
+    }
+}
