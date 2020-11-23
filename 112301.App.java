6084cf744baa18f0c205912cfd57b85a76a3e380
19980402 <74659985+19980402@users.noreply.github.com>
2020-11-23 11:50:57 +0800

    使用java设计一个随机坐标生成程序

--- 112301.App.java
+++ 112301.App.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class suiji {
     public static void main(String[] args) {
+        
         Scanner s=new Scanner(System.in);
         while (true){
             int a=s.nextInt();
