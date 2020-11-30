9e79ccecf880860af59f2205a10ceeb3524110f3
19980402 <74659985+19980402@users.noreply.github.com>
2020-11-30 11:58:48 +0800

    java接口方法

---
+++ 113002.接口.R.java
@@ -0,0 +1,12 @@
+public class R implements App {
+    public static void main(String[] args) {
+        R i=new R();
+        System.out.println(i.boy_filePath+"\n"+i.girl_filePath);
+        for(int k=0;k<i.array2d[0].length;k++){
+            for(int j=0;j<i.array2d[0].length;j++){
+                System.out.println(i.array2d[k][j]+",");
+            }
+            System.out.println();
+        }
+    }
+}
