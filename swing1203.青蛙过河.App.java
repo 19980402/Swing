ccb2b6b7c511921e94567f0d58a55816c32b7c2b
Jzya <74659985+Jzya@users.noreply.github.com>
2020-12-04 21:15:24 +0800

    青蛙过河小游戏

--- swing1203.青蛙过河.App.java
+++ swing1203.青蛙过河.App.java
@@ -45,9 +45,9 @@ public class App {
                 }
                 //第二只青蛙
                 if(s==1) {
-                    text2.setText(list[1][0]);
+                    text1.setText(list[1][0]);
                     text2.setText(list[1][1]);
-                    text2.setText(list[1][2]);
+                    text3.setText(list[1][2]);
                     if (textArea_A.getText().equals(list[1][0])) {
                         qingwa2.setBounds(400, 255, 128, 128);
                         textArea_A.setText("");
@@ -65,8 +65,8 @@ public class App {
                 }
                 //第三只青蛙
                 if(s==2) {
-                    text3.setText(list[2][0]);
-                    text3.setText(list[2][1]);
+                    text1.setText(list[2][0]);
+                    text2.setText(list[2][1]);
                     text3.setText(list[2][2]);
                     if (textArea_A.getText().equals(list[2][0])) {
                         qingwa3.setBounds(400, 255, 128, 128);
@@ -99,8 +99,6 @@ public class App {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setBounds(0,0,1024,600);
         frame.setVisible(true);
-
-
         textArea_A.setText("");
         textArea_A.setBounds(0,520,100,20);
         textArea_A.setBackground(Color.orange);
@@ -113,12 +111,9 @@ public class App {
         myPanel.add(qingwa3);
         myPanel.add(background);
 
-
-
         URL img1 = App.class.getResource("images/frog.png");
         qingwa1.setIcon(new ImageIcon(img1));
         qingwa1.setBounds(80,400,128,128);
-
 
         URL img2 = App.class.getResource("images/frog.png");
         qingwa2.setIcon(new ImageIcon(img2));
@@ -128,21 +123,17 @@ public class App {
         qingwa3.setIcon(new ImageIcon(img3));
         qingwa3.setBounds(280,400,128,128);
 
-
         URL img4 = App.class.getResource("images/background2.png");
         background.setIcon(new ImageIcon(img4));
         background.setBounds(0,0,1024,600);
+
         //设置字体
         text1.setBounds(430,330,100,50);
         text1.setText(list[0][0]);
         text1.setFont(f);
-
-
         text2.setBounds(280,150,100,50);
         text2.setText(list[0][1]);
         text2.setFont(f);
-
-
         text3.setBounds(600,80,100,50);
         text3.setText(list[0][2]);
         text3.setFont(f);
